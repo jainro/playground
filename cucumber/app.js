b9e02066850589913df2088cbee3a3171d64c71f
Rohit Jain <rohit1976@gmail.com>
2015-07-03 06:48:13 +0100

    Create app.js

---
+++ cucumber/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'myApp.dashboard',
+    'ui.bootstrap'
+]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/dashboard'});
+    }]);
+
+
+
+
+
